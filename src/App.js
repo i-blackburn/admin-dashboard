b15ae418719467cd3d51c4134e8dc4b459441a91
Ian Blackburn <ianblackburndeveloper@gmail.com>
2022-05-17 21:07:12 +0100

    homepage dark mode

--- src/App.js
+++ src/App.js
@@ -5,33 +5,36 @@ import Login from './pages/login/Login';
 import Single from './pages/single/Single';
 import New from './pages/new/New';
 import { productInputs, userInputs } from './formsource';
+import './style/dark.scss';
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/">
-					<Route index element={<Home />} />
-					<Route path="login" element={<Login />} />
-					<Route path="users">
-						<Route index element={<List />} />
-						<Route path=":userId" element={<Single />} />
-						<Route
-							path="new"
-							element={<New inputs={userInputs} title="Add New User" />}
-						/>
+		<div className="app dark">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/">
+						<Route index element={<Home />} />
+						<Route path="login" element={<Login />} />
+						<Route path="users">
+							<Route index element={<List />} />
+							<Route path=":userId" element={<Single />} />
+							<Route
+								path="new"
+								element={<New inputs={userInputs} title="Add New User" />}
+							/>
+						</Route>
+						<Route path="products">
+							<Route index element={<List />} />
+							<Route path=":productId" element={<Single />} />
+							<Route
+								path="new"
+								element={<New inputs={productInputs} title="Add New Product" />}
+							/>
+						</Route>
 					</Route>
-					<Route path="products">
-						<Route index element={<List />} />
-						<Route path=":productId" element={<Single />} />
-						<Route
-							path="new"
-							element={<New inputs={productInputs} title="Add New Product" />}
-						/>
-					</Route>
-				</Route>
-			</Routes>
-		</BrowserRouter>
+				</Routes>
+			</BrowserRouter>
+		</div>
 	);
 }
 
